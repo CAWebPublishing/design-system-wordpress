58472e8db8566da116fc50cff510975acac4835f
Danny Guzman <guzmanjd86@gmail.com>
2022-05-16 08:55:14 -0700

    Added filters/functions to their appropriate file designation Added WordPress Plugin Updater

---
+++ inc/functions.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Design System Helper Functions
+ *
+ * @package cagov-design-system
+ */
+
+/**
+ * Load Minified Version of a file
+ *
+ * @param  string $f File to load.
+ * @param  string $ext Extension of file, default css.
+ *
+ * @return string
+ */
+function cagov_ds_gutenberg_get_min_file( $f, $ext = 'css' ) {
+	// if not debugging and a minified version exists load it.
+	if ( CAGOV_DESIGN_SYSTEM_GUTENBERG__DEBUG && file_exists( CAGOV_DESIGN_SYSTEM_GUTENBERG . str_replace( ".$ext", ".min.$ext", $f ) ) ) {
+		return CAGOV_DESIGN_SYSTEM_GUTENBERG_URI . str_replace( ".$ext", ".min.$ext", $f );
+	} else {
+		return CAGOV_DESIGN_SYSTEM_GUTENBERG_URI . $f;
+	}
+}
